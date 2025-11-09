8f5dedc883ca378163879d1ff80bc620d4620a36
HAFIS-DAVIES <hafis.davies01@gmail.com>
2025-11-08 22:39:57 -0700

    gcp cloudrun

--- src/custom_exception.py
+++ src/custom_exception.py
@@ -10,7 +10,7 @@ class CustomException(Exception):
 
     @staticmethod
     def get_detailed_error_message(error_message, error_detail: sys) -> str:
-        _, _, exc_tb = error_detail.exc_info()
+        _, _, exc_tb = traceback.sys.exc_info()
         line_number = exc_tb.tb_lineno
         file_name = exc_tb.tb_frame.f_code.co_filename
         detailed_message = f"Error occurred in script: {file_name} at line number: {line_number} with message: {error_message}"
