4477d29dfb4e4bbbfe8f393c438438d1fd33d5c4
HAFIS-DAVIES <hafis.davies01@gmail.com>
2025-11-08 23:25:47 -0700

    5000

--- application.py
+++ application.py
@@ -34,5 +34,5 @@ def index():
     return render_template('index.html',prediction = None)
 
 if __name__== "__main__":
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
